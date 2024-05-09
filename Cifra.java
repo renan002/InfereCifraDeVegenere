f912feda4cc3d9418b75ff1869d2e1c4fbc9ee57
Renan Mastropaolo <re.gon.mas@gmail.com>
2024-05-09 20:50:35 -0300

    V2

--- Cifra.java
+++ Cifra.java
@@ -1,74 +1,70 @@
+import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 
 public class Cifra {
 
     static String alfabeto = "abcdefghijklmnopqrstuvwxyz";
+    static char[] alfabetos = alfabeto.toCharArray();
+    static String chave;
+    static final int tamanhoTextos = 16;
 
     public static void main(String[] args) throws IOException {
 
-        char[] alfabetos = alfabeto.toCharArray();
-
         String texto1 = Files.readAllLines(Paths.get("textos/Texto1.txt"), Charset.defaultCharset()).get(0).toLowerCase();
-        System.out.println(texto1);
         String texto2 = Files.readAllLines(Paths.get("textos/Texto2.txt"), Charset.defaultCharset()).get(0).toLowerCase();
-        System.out.println(texto2);
         String texto3 = Files.readAllLines(Paths.get("textos/Texto3.txt"), Charset.defaultCharset()).get(0).toLowerCase();
-        System.out.println(texto3);
 
-        String chave = Files.readAllLines(Paths.get("textos/chave.txt"), Charset.defaultCharset()).get(0).toLowerCase();
+        retornChaveCompleta();
 
-        char a = 'a';
-        char z = 'z';
-        char ii = 'i';
-        char c = 'c';
-        char l = 'k';
+        gravarCifra(texto1, "cifras/cifra1.txt");
+        gravarCifra(texto2, "cifras/cifra2.txt");
+        gravarCifra(texto3, "cifras/cifra3.txt");
 
-        System.out.println(Character.getNumericValue(a));
-        System.out.println(Character.getNumericValue(z));
+    }
 
-        System.out.println(Character.getNumericValue('i'));
-        System.out.println(Character.getNumericValue('c'));
+    private static void gravarCifra(String texto, String nomeArquivo) throws IOException {
+        FileWriter cifra = new FileWriter(nomeArquivo);
+        cifra.write(retornaTextoCifrado(texto));
+        cifra.close();
+    }
 
-        System.out.println(chave);
+    private static void retornChaveCompleta() throws IOException {
+        String chaveTmp = Files.readAllLines(Paths.get("textos/chave.txt"), Charset.defaultCharset()).get(0).toLowerCase();
+
         StringBuilder sb = new StringBuilder();
-        sb.append(chave);
-        char[] letrasDaChave = chave.toCharArray();
-        char[] letrasDoTexto1 = texto1.toCharArray();
-        char[] letrasDoTexto2 = texto2.toCharArray();
-        char[] letrasDoTexto3 = texto3.toCharArray();
+        sb.append(chaveTmp);
+        char[] letrasDaChave = chaveTmp.toCharArray();
 
-        while (sb.length() < texto1.length()) {
+        while (sb.length() < tamanhoTextos) {
             sb.append(letrasDaChave[sb.length() % 5]);
         }
         chave = sb.toString();
-
-        System.out.println(chave);
-
-        char chave0 = chave.charAt(0);
-        char texto10 = texto1.charAt(0);
-
-        int r = chave0 + texto10;
-
-        while (r > 123) r-=25;
-
-        System.out.println(getCharIndex('z'));
     }
 
     static int getCharIndex(char c) {
-        int asciiValue = (int) c;
 
-        if (asciiValue >= 'a' && asciiValue <= 'z') {
-            return asciiValue - 'a';
-        } else if (asciiValue >= 'A' && asciiValue <= 'Z') {
-            return asciiValue - 'A';
+        if (c >= 'a' && c <= 'z') {
+            return c - 'a';
+        } else if (c >= 'A' && c <= 'Z') {
+            return c - 'A';
         } else {
             return -1;
         }
+    }
+
+    static String retornaTextoCifrado(String texto) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < texto.length(); i++) {
+
+            int r = getCharIndex(chave.charAt(i)) + getCharIndex(texto.charAt(i));
+
+            while (r >= 26) r-=26;
+            sb.append(alfabetos[r]);
+        }
+
+        return sb.toString();
     }
 }
