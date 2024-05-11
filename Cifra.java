a9966cab78f000e9e37b6b1bb5a25ff0046aad3d
Renan Mastropaolo <re.gon.mas@gmail.com>
2024-05-10 23:05:40 -0300

    v3

--- Cifra.java
+++ Cifra.java
@@ -10,6 +10,7 @@ public class Cifra {
     static char[] alfabetos = alfabeto.toCharArray();
     static String chave;
     static int tamanhoTextos = 0;
+    static int[] quantidadeLetras = new int[26];
 
     public static void main(String[] args) throws IOException {
 
@@ -19,16 +20,63 @@ public class Cifra {
 
         tamanhoTextos = texto1.length();
 
-        retornChaveCompleta();
+        retornaChaveCompleta();
 
         gravarCifra(texto1, "cifras/cifra1.txt");
         gravarCifra(texto2, "cifras/cifra2.txt");
         gravarCifra(texto3, "cifras/cifra3.txt");
 
+        inferirCifra();
     }
 
-    private static void inferirCifra() {
+    private static void inferirCifra() throws IOException {
+        char[] frequenciaLetras = new char[] {'a', 'e', 'o', 's', 'r', 'i', 'n', 'd', 'm', 'u', 't', 'c', 'l', 'p', 'v', 'g', 'h', 'q', 'b', 'f', 'z', 'j', 'x', 'k', 'w', 'y'};
+        String cifra1 = Files.readAllLines(Paths.get("cifras/cifra1.txt"), Charset.defaultCharset()).get(0).toLowerCase();
+        String cifra2 = Files.readAllLines(Paths.get("cifras/cifra2.txt"), Charset.defaultCharset()).get(0).toLowerCase();
+        String cifra3 = Files.readAllLines(Paths.get("cifras/cifra3.txt"), Charset.defaultCharset()).get(0).toLowerCase();
 
+        popularArray(cifra1.toCharArray());
+        popularArray(cifra2.toCharArray());
+        popularArray(cifra3.toCharArray());
+
+        for (int i = 0; i < quantidadeLetras.length; i++) {
+            System.out.println(i + " " + quantidadeLetras[i]);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println("========================================================");
+
+        for (int i = 0; i < quantidadeLetras.length; i++) {
+            int maior = 0;
+            int index = -1;
+            for (int j = 0; j < quantidadeLetras.length; j++) {
+                //System.out.println(j+" "+quantidadeLetras[j]);
+                if (quantidadeLetras[j] >= maior) {
+                    maior = quantidadeLetras[j];
+                    index = j;
+                }
+            }
+            quantidadeLetras[index] = 0;
+
+            System.out.println(i+" "+getCharByIndex(index) + " " + maior);
+            //System.out.println((char) ('a'-1));
+
+        }
+
+
+
+    }
+
+    private static int diferencaEntreChars(char a, char b) {
+        return getIndexByChar(a) - getIndexByChar(b);
+    }
+
+    private static void popularArray(char[] arrayCifra) {
+        for (int i = 0; i < tamanhoTextos; i++) {
+            int index = getIndexByChar(arrayCifra[i]);
+            quantidadeLetras[index] += 1;
+        }
     }
 
     private static void gravarCifra(String texto, String nomeArquivo) throws IOException {
@@ -37,7 +85,7 @@ public class Cifra {
         cifra.close();
     }
 
-    private static void retornChaveCompleta() throws IOException {
+    private static void retornaChaveCompleta() throws IOException {
         String chaveTmp = Files.readAllLines(Paths.get("chaves/chave.txt"), Charset.defaultCharset()).get(0).toLowerCase();
 
         StringBuilder sb = new StringBuilder();
@@ -50,7 +98,7 @@ public class Cifra {
         chave = sb.toString();
     }
 
-    static int getCharIndex(char c) {
+    static int getIndexByChar(char c) {
 
         if (c >= 'a' && c <= 'z') {
             return c - 'a';
@@ -61,11 +109,26 @@ public class Cifra {
         }
     }
 
+    static char getCharByIndex(int c) {
+
+        if (c >= 0 && c <= 25) {
+            return (char) ('a' + c);
+        } else if (c >= 'A' && c <= 'Z') {
+            return (char) ('A' + c);
+        } else {
+            return Character.highSurrogate(483758934);
+        }
+    }
+
     static String retornaTextoCifrado(String texto) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < texto.length(); i++) {
 
-            int r = getCharIndex(chave.charAt(i)) + getCharIndex(texto.charAt(i));
+            int r = getIndexByChar(chave.charAt(i)) + getIndexByChar(texto.charAt(i));
+            System.out.println("chave "+getIndexByChar(chave.charAt(i)));
+            System.out.println("texto "+getIndexByChar(texto.charAt(i)));
+            System.out.println(i);
+            System.out.println(texto.charAt(i));
 
             while (r >= 26) r-=26;
             sb.append(alfabetos[r]);
