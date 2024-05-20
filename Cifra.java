cc51b811bbc9cb93246fddb1ead6dac152de4bb1
Renan_Mastropaolo <renan_mastropaolo@dell.com>
2024-05-20 18:16:48 -0300

    duihdie

--- Cifra.java
+++ Cifra.java
@@ -12,7 +12,6 @@ public class Cifra {
     static String chave;
     static int tamanhoTextos = 0;
     static HashMap<String, ArrayList<Repeticoes>> indexRepeticoesCifradas = new HashMap<>();
-    static char[] frequenciaLetrasAlfabeto = new char[] {'a', 'e', 'o', 's', 'r', 'i', 'n', 'd', 'm', 'u', 't', 'c', 'l', 'p', 'v', 'g', 'h', 'q', 'b', 'f', 'z', 'j', 'x', 'k', 'w', 'y'};
     static int minRepeticoes = 4;
     static int quantidadeDeRepeticoesAnalisadas = 15;
 
@@ -22,7 +21,6 @@ public class Cifra {
     static String texto1;
     static String texto2;
     static String texto3;
-
 
     public static void main(String[] args) throws IOException {
 
@@ -44,11 +42,11 @@ public class Cifra {
 
         Map[] frequenciasTextos = new HashMap[textos.length];
 
+        System.out.printf("Inferindo cifra... Time: %s\n", new Date());
+
         for (int i = 0; i < textos.length; i++) {
             frequenciasTextos[i] = calcularFrequenciaLetras(textos[i]);
         }
-
-        System.out.println("===============================================================================================");
 
         int tamanhoFinal = estimarTamanhoDaChave(cifras);
 
@@ -56,7 +54,16 @@ public class Cifra {
 
         int[][] fatiasCifradas = fatiarTextos(cifras, tamanhoFinal);
 
-        StringBuilder sb1 = new StringBuilder();
+        String chaveInicial = obterChaveInicial(fatiasCifradas);
+
+        String chaveFinal = inferirChaveHill(cifras, frequenciasTextos, chaveInicial);
+
+        System.out.printf("Chave Final: %s\n", chaveFinal);
+
+        System.out.printf("Termino da Inferencia Time: %s\n", new Date());
+    }
+
+    private static String obterChaveInicial(int[][] fatiasCifradas) {
         StringBuilder sb = new StringBuilder();
 
         for (int k = 0; k < fatiasCifradas.length; k++) {
@@ -65,21 +72,15 @@ public class Cifra {
             int freqMaior = 0;
             for (int i1 = 0; i1 < i.length; i1++) {
                 int j = i[i1];
-                System.out.printf("Key: %s - char: %c - Quantidade: %s \n", k, getCharByIndex(i1), j);
+                //System.out.printf("Key: %s - char: %c - Quantidade: %s \n", k, getCharByIndex(i1), j);
                 if (j>freqMaior) {
                     freqMaior = j;
                     charMaior = getCharByIndex(i1);
                 }
             }
-            sb1.append(obterCharFinal(charMaior, frequenciaLetrasAlfabeto[k]));
             sb.append(charMaior);
-            System.out.println("=======================================================================");
         }
-        //System.out.println(sb1);
-        System.out.println(sb);
-
-        inferirChaveHill(cifras, frequenciasTextos, sb.toString());
-
+        return sb.toString();
     }
 
     private static int estimarTamanhoDaChave(String[] cifras) {
@@ -120,17 +121,6 @@ public class Cifra {
         return (int) key0;
     }
 
-    static char obterCharFinal(char a, char b) {
-        int ai = getIndexByChar(a);
-        int bi = getIndexByChar(b);
-
-        int r = Math.max(ai, bi) - Math.min(ai, bi);
-
-        //while (r >= 26) r-=26;
-
-        return getCharByIndex(r);
-    }
-
     private static int mdcEntreIndexes(List<Repeticoes> lista) {
         int[] distancias = new int[lista.size()];
         for (int i = lista.size()-1; i > 0; i--) {
@@ -160,14 +150,13 @@ public class Cifra {
     }
 
     private static HashMap<String, Integer> obterRepticoesCifradas(String[] cifras) {
-        System.out.printf("Obtendo repetições... Time: %s\n", new Date());
+
         HashMap<String, Integer> retorno = new HashMap<>();
         for (String cifra : cifras) {
             for (int j = minRepeticoes; j < 50; j++) {
                 for (int i = 0; i < cifra.length() - j + 1; i++) {
                     String sequencia = cifra.substring(i, i + j);
 
-                    // Verificar se a sequência já está no mapa
                     if (retorno.containsKey(sequencia)) {
                         retorno.put(sequencia, retorno.get(sequencia) + 1);
                         indexRepeticoesCifradas.get(sequencia).add(new Repeticoes(i, i + j));
@@ -181,8 +170,6 @@ public class Cifra {
             }
         }
 
-        System.out.printf("Repetições finalizadas Time: %s\n", new Date());
-
         return sortByValueS(retorno);
     }
 
@@ -194,7 +181,6 @@ public class Cifra {
             int init = 0;
             for (int i = 0; i < quantidadeFatias; i++) {
                 fatias[i] = cifra.substring(init, init += tamanhoChave);
-                System.out.println(fatias[i]);
             }
 
             for (int i = 0; i < tamanhoChave; i++) {
@@ -217,57 +203,84 @@ public class Cifra {
 
     private static String inferirChaveHill(String[] textosCifrados, Map<Character, Integer>[] frequenciasLetras, String chaveInicial) {
 
-        // Definir parâmetros do Hill-climbing
-        int maxIteracoes = 1000; // Número máximo de iterações
-        double taxaMutacao = 0.1; // Probabilidade de mutação de um caractere na chave
+        int maxIteracoes = 10;
+        double chiQuadradoTotal = 0.0;
+        int offset = 0;
+        do {
+            Set<Integer> aumentados = new HashSet<>();
+            Set<Integer> diminuidos = new HashSet<>();
 
-        // Realizar o Hill-climbing
-        for (int iteracao = 0; iteracao < maxIteracoes; iteracao++) {
-            // Avaliar a qualidade da chave candidata em todos os textos
-            double chiQuadradoTotal = 0.0;
-            for (int i = 0; i < textosCifrados.length; i++) {
-                String textoCifrado = textosCifrados[i];
-                chiQuadradoTotal += calcularChiQuadrado(frequenciasLetras[i], descriptografarVigenere(textoCifrado, chaveInicial));
-            }
+            for (int iteracao = 0; iteracao < maxIteracoes; iteracao++) {
 
-            // Tentar mutações na chave candidata
-            String chaveMutante = gerarChaveMutante(chaveInicial, taxaMutacao);
-            double chiQuadradoMutanteTotal = 0.0;
-            for (int i = 0; i < textosCifrados.length; i++) {
-                String textoCifrado = textosCifrados[i];
-                chiQuadradoMutanteTotal += calcularChiQuadrado(frequenciasLetras[i], descriptografarVigenere(textoCifrado, chaveMutante));
-            }
+                chiQuadradoTotal = 0.0;
+                for (int i = 0; i < textosCifrados.length; i++) {
+                    String textoCifrado = textosCifrados[i];
+                    chiQuadradoTotal += calcularChiQuadrado(frequenciasLetras[i], descriptografarVigenere(textoCifrado, chaveInicial));
+                }
 
-            // Se a mutação melhorar o chi-quadrado total, atualizar a chave candidata
-            if (chiQuadradoMutanteTotal < chiQuadradoTotal) {
-                chaveInicial = chaveMutante;
-                System.out.println("Iteração: " + iteracao + ", Chi-Quadrado Total: " + chiQuadradoMutanteTotal + ", Chave: " + chaveInicial);
+                for (int j = 0; j < chaveInicial.length(); j++) {
+                    String chaveMutanteMenos = "";
+                    String chaveMutanteMais = "";
+                    if (!aumentados.contains(j) && !diminuidos.contains(j)) {
+                        chaveMutanteMenos = gerarChaveMutante(chaveInicial, j, false, 1+ offset);
+                        chaveMutanteMais = gerarChaveMutante(chaveInicial, j, true, 1+ offset);
+                    } else if (aumentados.contains(j)) {
+                        chaveMutanteMenos = gerarChaveMutante(chaveInicial, j, true, 1+ offset);
+                        chaveMutanteMais = gerarChaveMutante(chaveInicial, j, true, 2+ offset);
+                    } else if (diminuidos.contains(j)) {
+                        chaveMutanteMenos = gerarChaveMutante(chaveInicial, j, false, 1+ offset);
+                        chaveMutanteMais = gerarChaveMutante(chaveInicial, j, false, 2+ offset);
+                    }
+                    double chiQuadradoMutanteTotalMais = 0.0;
+                    double chiQuadradoMutanteTotalMenos = 0.0;
+                    for (int i = 0; i < textosCifrados.length; i++) {
+                        String textoCifrado = textosCifrados[i];
+                        chiQuadradoMutanteTotalMais += calcularChiQuadrado(frequenciasLetras[i], descriptografarVigenere(textoCifrado, chaveMutanteMais));
+                    }
+                    for (int i = 0; i < textosCifrados.length; i++) {
+                        String textoCifrado = textosCifrados[i];
+                        chiQuadradoMutanteTotalMenos += calcularChiQuadrado(frequenciasLetras[i], descriptografarVigenere(textoCifrado, chaveMutanteMenos));
+                    }
+
+                    if (chiQuadradoMutanteTotalMais < chiQuadradoTotal && chiQuadradoMutanteTotalMais < chiQuadradoMutanteTotalMenos) {
+                        chaveInicial = chaveMutanteMais;
+                        if (!diminuidos.contains(j))
+                            aumentados.add(j);
+                        //System.out.println("Offet: "+offset+" MAIS - Iteração: " + iteracao + ", Chi-Quadrado Total: " + chiQuadradoMutanteTotalMais + ", Chave: " + chaveInicial);
+                        chiQuadradoTotal = chiQuadradoMutanteTotalMais;
+
+                    } else if (chiQuadradoMutanteTotalMenos < chiQuadradoTotal && chiQuadradoMutanteTotalMenos < chiQuadradoMutanteTotalMais) {
+                        chaveInicial = chaveMutanteMenos;
+                        if (!aumentados.contains(j))
+                            diminuidos.add(j);
+                        //System.out.println("Offet: "+offset+" MENOS - Iteração: " + iteracao + ", Chi-Quadrado Total: " + chiQuadradoMutanteTotalMenos + ", Chave: " + chaveInicial);
+                        chiQuadradoTotal = chiQuadradoMutanteTotalMenos;
+                    } else {
+                        //diminuidos.remove(j);
+                        //aumentados.remove(j);
+                    }
+                }
             }
-        }
+            offset++;
+
+            if (offset>tamanhoTextos) break;
+        } while (chiQuadradoTotal!=0);
 
         return chaveInicial;
     }
-    // Método para gerar uma chave candidata aleatória
-    private static String gerarChaveCandidataAleatoria() {
-        StringBuilder chaveCandidata = new StringBuilder();
-        for (int i = 0; i < 56; i++) {
-            chaveCandidata.append(alfabetos[new Random().nextInt(alfabetos.length)]);
-        }
-        return chaveCandidata.toString();
-    }
 
-    // Método para gerar uma chave mutante a partir da chave candidata atual
-    private static String gerarChaveMutante(String chaveCandidata, double taxaMutacao) {
+    private static String gerarChaveMutante(String chaveCandidata, int alterarIndex, boolean aumentar, int quantidade) {
         StringBuilder chaveMutante = new StringBuilder(chaveCandidata);
-        for (int i = 0; i < chaveCandidata.length(); i++) {
-            if (Math.random() < taxaMutacao) {
-                chaveMutante.setCharAt(i, alfabetos[new Random().nextInt(alfabetos.length)]);
-            }
+        char c = chaveCandidata.charAt(alterarIndex);
+        if (!aumentar ) {
+            chaveMutante.setCharAt(alterarIndex, alfabetos[getIndexByIndexCiclico(getIndexByChar(c)-quantidade)]);
+        } else {
+            chaveMutante.setCharAt(alterarIndex, alfabetos[getIndexByIndexCiclico(getIndexByChar(c)+quantidade)]);
         }
+
         return chaveMutante.toString();
     }
 
-    // Método para descriptografar o texto cifrado usando a chave fornecida
     private static String descriptografarVigenere(String textoCifrado, String chave) {
         StringBuilder textoDescriptografado = new StringBuilder();
 
@@ -280,69 +293,26 @@ public class Cifra {
         return textoDescriptografado.toString();
     }
 
-    // Método para calcular o chi-quadrado entre duas frequências de letras
     private static double calcularChiQuadrado(Map<Character, Integer> frequenciaEsperada, String texto) {
         double chiQuadradoTotal = 0.0;
         Map<Character, Integer> frequenciaObservada = calcularFrequenciaLetras(texto);
         for (char letra : alfabetos) {
-            int frequenciaEsperadaLetra = frequenciaEsperada.getOrDefault(letra, 0); //TODO frequenciaEsperadaLetra pode ser 0 se não tiver nenhuma ocorrencia da letra no texto
-            int frequenciaObservadaLetra = frequenciaObservada.getOrDefault(letra, 0);
+            int frequenciaEsperadaLetra = frequenciaEsperada.getOrDefault(letra, 1);
+            int frequenciaObservadaLetra = frequenciaObservada.getOrDefault(letra, 1);
             double contribuicaoChiQuadrado = Math.pow((frequenciaEsperadaLetra - frequenciaObservadaLetra), 2) / frequenciaEsperadaLetra;
             chiQuadradoTotal += contribuicaoChiQuadrado;
         }
         return chiQuadradoTotal;
     }
 
-
-
-    public static HashMap<Character, Float> sortByValueF(HashMap<Character, Float> hm)
-    {
-        // Create a list from elements of HashMap
-        List<Map.Entry<Character, Float> > list =
-                new LinkedList<>(hm.entrySet());
-
-        // Sort the list
-        list.sort(Map.Entry.comparingByValue());
-
-        Collections.reverse(list);
-
-        // put data from sorted list to hashmap
-        HashMap<Character, Float> temp = new LinkedHashMap<>();
-        for (Map.Entry<Character, Float> aa : list) {
-            temp.put(aa.getKey(), aa.getValue());
-        }
-        return temp;
-    }
-
-    private static HashMap<Character, Integer> sortByValue(HashMap<Character, Integer> hm) {
-        // Create a list from elements of HashMap
-        List<Map.Entry<Character, Integer> > list =
-                new LinkedList<>(hm.entrySet());
-
-        // Sort the list
-        list.sort(Map.Entry.comparingByValue());
-
-        Collections.reverse(list);
-
-        // put data from sorted list to hashmap
-        HashMap<Character, Integer> temp = new LinkedHashMap<>();
-        for (Map.Entry<Character, Integer> aa : list) {
-            temp.put(aa.getKey(), aa.getValue());
-        }
-        return temp;
-    }
-
     private static HashMap<String, Integer> sortByValueS(HashMap<String, Integer> hm) {
-        // Create a list from elements of HashMap
         List<Map.Entry<String, Integer> > list =
                 new LinkedList<>(hm.entrySet());
 
-        // Sort the list
         list.sort(Map.Entry.comparingByValue());
 
         Collections.reverse(list);
 
-        // put data from sorted list to hashmap
         HashMap<String, Integer> temp = new LinkedHashMap<>();
         for (Map.Entry<String, Integer> aa : list) {
             temp.put(aa.getKey(), aa.getValue());
@@ -351,16 +321,13 @@ public class Cifra {
     }
 
     private static HashMap<Integer, Integer> sortByValueII(HashMap<Integer, Integer> hm) {
-        // Create a list from elements of HashMap
         List<Map.Entry<Integer, Integer> > list =
                 new LinkedList<>(hm.entrySet());
 
-        // Sort the list
         list.sort(Map.Entry.comparingByValue());
 
         Collections.reverse(list);
 
-        // put data from sorted list to hashmap
         HashMap<Integer, Integer> temp = new LinkedHashMap<>();
         for (Map.Entry<Integer, Integer> aa : list) {
             temp.put(aa.getKey(), aa.getValue());
@@ -400,6 +367,19 @@ public class Cifra {
             return c - 'A';
         } else {
             return -1;
+        }
+    }
+
+    static int getIndexByIndexCiclico(int c) {
+
+        if (c >= 0 && c <= 25) {
+            return c;
+        } else if (c < 0 ) {
+            while (c < 0) c+=26;
+            return c;
+        } else {
+            while (c >= 26) c-=26;
+            return c;
         }
     }
 
